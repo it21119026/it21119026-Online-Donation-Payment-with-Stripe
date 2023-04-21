b9db640b50d2ed9cb3fbcfe868de4ec85bd2b6d0
Kolitha <it21119026@my.sliit.lk>
2023-04-21 13:46:58 +0530

    1 push

---
+++ backend/products.js
@@ -0,0 +1,103 @@
+const products = [
+  {
+    id: 1,
+    name: "pic_6",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055140/Donations/pic13-01_jfr304.jpg",
+  },
+  {
+    id: 2,
+    name: "pic13-01",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055139/Donations/pic_6-01_flhsot.jpg",
+  },
+  {
+    id: 3,
+    name: "7",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055139/Donations/pic7-01_ah7gw6.jpg",
+  },
+  {
+    id: 2,
+    name: "8",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055140/Donations/pic8-01_amu1pq.jpg",
+  },
+  {
+    id: 3,
+    name: "14",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055141/Donations/pic14-01_pchak6.jpg",
+  },
+  {
+    id: 1,
+    name: "11",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055140/Donations/pic11-01_bshcos.jpg",
+  },
+  {
+    id: 2,
+    name: "09",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055140/Donations/pic9-01_rwnp3p.jpg",
+  },
+  {
+    id: 3,
+    name: "3",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055139/Donations/pic3-01_yzomdc.jpg",
+  },
+  {
+    id: 2,
+    name: "8",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055140/Donations/pic10-01_smnq0g.jpg",
+  },
+  {
+    id: 3,
+    name: "14",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055139/Donations/pic4-01_p0fta9.jpg",
+  },
+  {
+    id: 3,
+    name: "14",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+    "https://res.cloudinary.com/dhll9ffus/image/upload/v1682055139/Donations/pic2-01_dhxf9j.jpg",
+  }
+];
+
+module.exports = products;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+import { logoutUser } from "../slices/authSlice";
+import { toast } from "react-toastify";
+
+const NavBar = () => {
+  const dispatch = useDispatch();
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
+  const auth = useSelector((state) => state.auth);
+
+  return (
+    <nav className="nav-bar">
+      <Link to="/">
+        <h2>Donation</h2>
+      </Link>
+      <Link to="/cart">
+        <div className="nav-bag">
+        <svg 
+    xmlns="http://www.w3.org/2000/svg"
+	 width="35" 
+	height="35" 
+	fill="currentColor"
+	 class="bi bi-bag-heart-fill" 
+	viewBox="0 0 16 16">
+  <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+</svg>
+          <span className="bag-quantity">
+            <span>{cartTotalQuantity}</span>
+          </span>
+        </div>
+      </Link>
+      {auth._id ? (
+        <Logout
+          onClick={() => {
+            dispatch(logoutUser(null));
+            toast.warning("Logged out!", { position: "bottom-left" });
+          }}
+        >
+          Logout
+        </Logout>
+      ) : (
+        <AuthLinks>
+          <Link to="/login">Login</Link>
+          <Link to="register">Register</Link>
+        </AuthLinks>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
+
+const AuthLinks = styled.div`
+  a {
+    &:last-child {
+      margin-left: 2rem;
+    }
+  }
+`;
+
+const Logout = styled.div`
+  color: white;
+  cursor: pointer;
+`;
